043e6a740eee0bbdb66bdd801ccf41ce7b247caa
KoretsuNobuyasu <einsatzkommandocobra.1996@gmail.com>
2018-10-08 22:59:39 +0900

    天気予報プログラム完成

--- Python/Automate_The_Boring_Stuff_With_Python/weather.py
+++ Python/Automate_The_Boring_Stuff_With_Python/weather.py
@@ -6,7 +6,6 @@
 
 # import
 import json, requests, sys
-# class
 
 
 EXIT_SUCCESS = 0
@@ -14,25 +13,43 @@ EXIT_FAILURE = 1
 #openweathermap.org API key
 APPID='a584bd2a6688a2c48c50b3775c6d691b'
 
-#openweathermap.org の　APIからJSONデータをダウンロード
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q={}&cnt=3&appid={}'.format(location, APPID)
-
-
-
 def main():
 
     if len(sys.argv) < 2:
         print('Usage: weather.py location')
+        print('場所を入力してください')
         sys.exit()
     location = ' '.join(sys.argv[1:])
-    get_info()
+    # openweathermap.org 　JSON DATA Download from API
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&cnt=3&appid={}'.format(location, APPID)
+    data = loading(location, get_info(url))
+
+    # it show weather info about select location
+    w = data['list']
+    print('{}の現在の天気'.format(location))
+    print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+    print()
+    print('明日:')
+    print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+    print()
+    print('明後日:')
+    print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
 
     return  EXIT_SUCCESS
 
 
-def get_info():
+def get_info(url):
     response = requests.get(url)
     response.raise_for_status()
+    return response
+
+def loading(location, response):
+    #JSON Data to Python variable
+    weather_data = json.loads(response.text)
+    return weather_data
+
+
+
 
 
 if __name__ == '__main__':
