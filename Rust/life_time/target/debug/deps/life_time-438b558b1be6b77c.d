28a6f27bd13d1e06fd64e9bc07b7522896289f0b
KoretsuNobuyasu <einsatzkommandocobra.1996@gmail.com>
2018-10-16 22:31:28 +0900

    life_time

---
+++ Rust/life_time/target/debug/deps/life_time-438b558b1be6b77c.d
@@ -0,0 +1,5 @@
+/Users/nobu/GitHub/Autoaprendizaje/Rust/life_time/target/debug/deps/life_time-438b558b1be6b77c: src/main.rs
+
+/Users/nobu/GitHub/Autoaprendizaje/Rust/life_time/target/debug/deps/life_time-438b558b1be6b77c.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/life_time/target/debug/life_time.d
@@ -0,0 +1,1 @@
+/Users/nobu/GitHub/Autoaprendizaje/Rust/life_time/target/debug/life_time.dSYM: /Users/nobu/GitHub/Autoaprendizaje/Rust/life_time/src/main.rs
