f8a27f57dc2f8f21d4942c61d96a426568189afc
KoretsuNobuyasu <einsatzkommandocobra.1996@gmail.com>
2018-11-23 16:25:43 +0900

    mandelbrot完成(予期していた画像と異なるので、どこかしら間違えてるけど...)

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -1,3 +1,5 @@
+extern crate crossbeam;
+
 extern crate num;
 use num::Complex;
 
@@ -19,7 +21,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T,T)> {
         None => None,
         Some(index) => {
             match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])){
-                (Ok(1), Ok(r)) => Some((l,r)),
+                (Ok(l), Ok(r)) => Some((l,r)),
                 _ => None
             }
         }
@@ -47,7 +49,7 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 #[test]
 fn test_parse_complex(){
     assert_eq!(parse_complex("1.25, -0.0625"),
-            Some(Complex {re: 1.25, im: -0.0625})),
+            Some(Complex {re: 1.25, im: -0.0625}));
     assert_eq!(parse_complex(",-0.0625"),None);
 }
 
@@ -102,7 +104,7 @@ use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
 
-fn write_image(filename: &str, picels: &[u8], bounds: (usize, usize))
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
     -> Result<(), std::io::Error>
 {
     let output = File::create(filename)?;
@@ -113,4 +115,57 @@ fn write_image(filename: &str, picels: &[u8], bounds: (usize, usize))
                     ColorType::Gray(8))?;
     
     Ok(())
+}
+
+use std::io::Write;
+
+fn main(){
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5{
+        writeln!(std::io::stderr(),
+                "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(),
+                "Example: {} mandel.png 1000x750 -1.20,0.35, -1,0.20",
+                args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = 
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner|{
+            for (i, band) in bands.into_iter().enumerate(){
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = 
+                    pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = 
+                    pixel_to_point(bounds, (bounds.0, top + height),
+                                    upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
